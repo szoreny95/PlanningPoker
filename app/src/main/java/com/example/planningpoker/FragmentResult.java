f7b15d5141d83c9d7a1a0924d17d7819e99065ce
TheFatKid <szoreny95@yahoo.com>
2019-10-24 18:56:45 +0300

    login screen slightly modified margins

--- app/src/main/java/com/example/planningpoker/FragmentResult.java
+++ app/src/main/java/com/example/planningpoker/FragmentResult.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentResult extends Fragment {
     @Nullable
