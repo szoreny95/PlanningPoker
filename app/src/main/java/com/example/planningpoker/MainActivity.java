c095663e558e72c699a7988b95c5104315e94c84
TheFatKid <szoreny95@yahoo.com>
2019-10-24 18:25:55 +0300

    1.3 branch.....

--- app/src/main/java/com/example/planningpoker/MainActivity.java
+++ app/src/main/java/com/example/planningpoker/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.planningpoker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,26 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void login_check(View view) {
         //ellenorzes
+        Intent intent = new Intent(this, VoteActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        startActivity(intent);
 
+    }
+/*
+    public void change_frag(View view)
+    {
         FragmentTransaction frag_trans = getSupportFragmentManager().beginTransaction();
-        frag_trans.add(R.id.fragment_container,new FragmentGrid());
+        frag_trans.replace(R.id.fragment_container,new FragmentResult());
         frag_trans.commit();
     }
-
-
-
-
-
-/*
-        Button btnLogin = findViewById(R.id.login_button);
-
-        btnLogin.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-
-            }
-        }
-    }*/
-
+*/
 }
