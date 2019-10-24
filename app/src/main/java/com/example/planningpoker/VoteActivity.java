2d473a9746313837d7001db971d44e748aeff25e
TheFatKid <szoreny95@yahoo.com>
2019-10-24 18:56:45 +0300

    login screen slightly modified margins

--- app/src/main/java/com/example/planningpoker/VoteActivity.java
+++ app/src/main/java/com/example/planningpoker/VoteActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class VoteActivity extends AppCompatActivity {
 
@@ -15,9 +16,23 @@ public class VoteActivity extends AppCompatActivity {
         FragmentTransaction frag_trans = getSupportFragmentManager().beginTransaction();
         frag_trans.add(R.id.fragment_container,new FragmentGrid());
         frag_trans.commit();
-
-
     }
 
+
+
+    public void change_frag(View view)
+    {
+        FragmentTransaction frag_trans = getSupportFragmentManager().beginTransaction();
+        frag_trans.replace(R.id.fragment_container,new FragmentResult());
+        frag_trans.commit();
+    }
+
+
+    public void change_frag_back(View view)
+    {
+        FragmentTransaction frag_trans = getSupportFragmentManager().beginTransaction();
+        frag_trans.replace(R.id.fragment_container,new FragmentGrid());
+        frag_trans.commit();
+    }
 
 }
