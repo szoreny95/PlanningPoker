500c0ba453cccf9fbeadfe820a47a065c0295f38
SzekeresBoroka <szekeresboroka@gmail.com>
2019-10-25 12:48:51 +0300

    set adapter and content to grid recycler view

--- app/src/main/java/com/example/planningpoker/FragmentGrid.java
+++ app/src/main/java/com/example/planningpoker/FragmentGrid.java
@@ -11,8 +11,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentGrid extends Fragment {
+    private RecyclerView recyclerView;
+    private GridAdapter gridAdapter;
+    private ArrayList<String> gridDataList = new ArrayList<>();
 
 
     @Nullable
@@ -20,8 +30,27 @@ public class FragmentGrid extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_grid,container,false);
 
+        recyclerView = v.findViewById(R.id.recyclerView);
+        gridAdapter = new GridAdapter(gridDataList);
+        RecyclerView.LayoutManager manager = new GridLayoutManager(getActivity(), 3);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(gridAdapter);
+        GridDataPrepare();
+
         return v;
     }
 
+    private void GridDataPrepare() {
+        gridDataList.add("\u2615");
+        gridDataList.add("1");
+        gridDataList.add("2");
+        gridDataList.add("3");
+        gridDataList.add("5");
+        gridDataList.add("8");
+        gridDataList.add("13");
+        gridDataList.add("20");
+        gridDataList.add("?");
+    }
 
 }
